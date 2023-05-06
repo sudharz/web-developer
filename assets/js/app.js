f63eee00cf8824c867be608b46e5532b23bd0298
sudharz <sudharshangopi@12gmail.com>
2023-05-06 17:36:36 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// Insert your code here.
+var slideIndex=0;
+showSlide();
+
+function showSlide(){
+    var1;
+    var slides = document.getElementsByClassName("c");{
+    for(i=o;i<slides.length;i++)
+    slides[i].getElementsByClassName.display = "name";
+}
+
+slidesIndex++;
+if (slideIndex > slides.length){
+    slideIndex = 1;
+
+}
+slides[slideIndex-1].style.display ="flex";
+setTimeout(showSlide,2500);
+}
